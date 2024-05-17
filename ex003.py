20b64d3d0d3a3a619701073dcd3a58a2e9a0140b
Pedro Nascimento <pedro@krooze.com.br>
2024-05-16 23:29:48 -0300

    add firsts exercises

---
+++ ex003.py
@@ -0,0 +1,4 @@
+number1 = int(input('Digite o primeiro numero '))
+number2 = int(input('Digite o segundo numero '))
+result = number1 + number2
+print('A soma entre {} e {} vale {}'.format(number1, number2, result))
